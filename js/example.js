02c173cafd1badce64615750471ce0247498ab84
Paul MAILLARDET <polo@etoilenoire>
2016-08-01 23:47:05 +0200

    Commenting code

--- js/example.js
+++ js/example.js
@@ -7,8 +7,9 @@
 
 var planttEx = angular.module("planttEx", ["plantt.module"]);
 
-planttEx.controller("planttExample", function($scope, $filter, $timeout){
+planttEx.controller("planttExample", function($scope, $timeout){
 
+	// Create the events list
 	$scope.events = [
 		{ id: 1, title: 'Opps Lala', type: 'normal', startDate: new Date(2016, 6-1, 21, 8), endDate: new Date(2016, 7-1, 4, 19) },
 		{ id: 2, title: 'Wouzah', type: 'normal', startDate: new Date(2016, 7-1, 14, 8), endDate: new Date(2016, 7-1, 15, 19) },
@@ -16,13 +17,14 @@ planttEx.controller("planttExample", function($scope, $filter, $timeout){
 		{ id: 4, title: 'Surpuissant', type: 'urgent', startDate: new Date(2016, 7-1, 19, 8), endDate: new Date(2016, 7-1, 21, 19) },
 		{ id: 5, title: 'Gniuk', type: 'urgent', startDate: new Date(2016, 7-1, 17, 8), endDate: new Date(2016, 7-1, 22, 19) },
 		{ id: 6, title: 'Gnark Gnark Gnark Gnark Gnark Gnark Gnark', type: 'urgent', startDate: new Date(2016, 7-1, 17, 8), endDate: new Date(2016, 7-1, 18, 19) },
-		{ id: 7, title: 'À cheval', type: 'normal', startDate: new Date(2016, 7-1, 29, 8), endDate: new Date(2016, 8-1, 2, 19) },
+		{ id: 7, title: 'En cours', type: 'normal', startDate: new Date(2016, 7-1, 29, 8), endDate: new Date(2016, 8-1, 2, 19) },
 		{ id: 8, title: 'AoutFirst', type: 'normal', startDate: new Date(2016, 8-1, 6, 8), endDate: new Date(2016, 8-1, 12, 19) },
 		{ id: 9, title: 'Aout Second one', type: 'urgent', startDate: new Date(2016, 8-1, 14, 8), endDate: new Date(2016, 8-1, 14, 19) },
 		{ id: 10, title: 'Aout Third event', type: 'urgent', startDate: new Date(2016, 8-1, 8, 8), endDate: new Date(2016, 8-1, 10, 19) },
 		{ id: 11, title: 'Buggy One', type: 'urgent', startDate: new Date(2016, 8-1, 2, 8), endDate: new Date(2016, 8-1, 6, 19) }
 	];
 
+	// Listen to the "daySelect" DOM event, to add a new event
 	$scope.$on('daySelect', function(e, date){
 		var name = prompt('New event title:');
 		if (!name) return;
@@ -42,6 +44,7 @@ planttEx.controller("planttExample", function($scope, $filter, $timeout){
 		}, 0);
 	});
 
+	// Listen to the "periodSelect" DOM event, to add a new event
 	$scope.$on('periodSelect', function(e, dates){
 		var name = prompt('New event title:');
 		if (!name) return;
@@ -60,6 +63,7 @@ planttEx.controller("planttExample", function($scope, $filter, $timeout){
 		}, 0);
 	});
 
+	// Listen to the "eventMove" DOM event, to store the new position of the event in time
 	$scope.$on('eventMove', function(e, event, deltaDays){
 		event.startDate = addDaysToDate(angular.copy(event.startDate), deltaDays);
 		event.endDate	= addDaysToDate(angular.copy(event.endDate), deltaDays);
