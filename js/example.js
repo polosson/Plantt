e56cc5cb94143d2d0f3cdda21ce369888f181834
Paul MAILLARDET <polo@etoilenoire>
2016-08-04 00:13:48 +0200

    Updated the example list, and added a screenshot

--- js/example.js
+++ js/example.js
@@ -15,18 +15,19 @@ planttEx.controller("planttExample", function($scope, $timeout){
 
 	// Create the events list
 	$scope.events = [
-		{ id: 1, title: 'Opps Lala', type: 'normal', startDate: new Date(2016, 6-1, 21, 8), endDate: new Date(2016, 7-1, 4, 19) },
-		{ id: 2, title: 'Wouzah', type: 'normal', startDate: new Date(2016, 7-1, 14, 8), endDate: new Date(2016, 7-1, 15, 19) },
-		{ id: 3, title: 'Hoydih', type: 'urgent', startDate: new Date(2016, 7-1, 21, 8), endDate: new Date(2016, 8-1, 1, 19) },
-		{ id: 4, title: 'Surpuissant', type: 'urgent', startDate: new Date(2016, 7-1, 19, 8), endDate: new Date(2016, 7-1, 21, 19) },
-		{ id: 5, title: 'Gniuk', type: 'urgent', startDate: new Date(2016, 7-1, 17, 8), endDate: new Date(2016, 7-1, 22, 19) },
-		{ id: 6, title: 'Gnark Gnark Gnark Gnark Gnark Gnark Gnark', type: 'urgent', startDate: new Date(2016, 7-1, 17, 8), endDate: new Date(2016, 7-1, 18, 19) },
-		{ id: 7, title: 'En cours', type: 'normal', startDate: new Date(2016, 7-1, 31, 8), endDate: new Date(2016, 8-1, 5, 19) },
-		{ id: 8, title: 'AoutFirst', type: 'normal', startDate: new Date(2016, 8-1, 8, 8), endDate: new Date(2016, 8-1, 14, 19) },
-		{ id: 9, title: 'Aout Second one', type: 'urgent', startDate: new Date(2016, 8-1, 16, 8), endDate: new Date(2016, 8-1, 16, 19) },
-		{ id: 10, title: 'Aout Third event', type: 'urgent', startDate: new Date(2016, 8-1, 9, 8), endDate: new Date(2016, 8-1, 11, 19) },
-		{ id: 11, title: 'Tout qui marche au top', type: 'urgent', startDate: new Date(2016, 8-1, 2, 8), endDate: new Date(2016, 8-1, 6, 19) },
-		{ id: 12, title: 'Mid-August Fiesta', type: 'normal', startDate: new Date(2016, 8-1, 16, 8), endDate: new Date(2016, 8-1, 21, 19) }
+		{ id: 1, title: 'Hello World', type: 'normal', startDate: new Date(2016, 6-1, 21, 8), endDate: new Date(2016, 7-1, 4, 19) },
+		{ id: 2, title: 'OK Junior, pent over', type: 'normal', startDate: new Date(2016, 7-1, 11, 8), endDate: new Date(2016, 7-1, 15, 19) },
+		{ id: 3, title: 'Running in the mountain', type: 'urgent', startDate: new Date(2016, 7-1, 18, 8), endDate: new Date(2016, 7-1, 20, 19) },
+		{ id: 4, title: 'July Ruby', type: 'urgent', startDate: new Date(2016, 7-1, 23, 8), endDate: new Date(2016, 7-1, 25, 19) },
+		{ id: 5, title: 'Old one', type: 'urgent', startDate: new Date(2016, 7-1, 17, 8), endDate: new Date(2016, 7-1, 29, 19) },
+		{ id: 6, title: 'Outdated event', type: 'urgent', startDate: new Date(2016, 7-1, 31, 8), endDate: new Date(2016, 8-1, 2, 19) },
+		{ id: 7, title: 'In progress, low priority', type: 'normal', startDate: new Date(2016, 8-1, 2, 8), endDate: new Date(2016, 8-1, 7, 19) },
+		{ id: 8, title: 'Full Week Holidays', type: 'normal', startDate: new Date(2016, 8-1, 8, 8), endDate: new Date(2016, 8-1, 14, 19) },
+		{ id: 9, title: 'Something to do soon', type: 'normal', startDate: new Date(2016, 8-1, 6, 8), endDate: new Date(2016, 8-1, 10, 19) },
+		{ id: 10, title: 'In progress, hi-priority', type: 'urgent', startDate: new Date(2016, 8-1, 4, 8), endDate: new Date(2016, 8-1, 8, 19) },
+		{ id: 11, title: 'Mid-August Fiesta on the beach', type: 'urgent', startDate: new Date(2016, 8-1, 15, 8), endDate: new Date(2016, 8-1, 21, 19) },
+		{ id: 12, title: '1 day', type: 'normal', startDate: new Date(2016, 8-1, 16, 8), endDate: new Date(2016, 8-1, 16, 19) },
+		{ id: 13, title: 'Testing', type: 'urgent', startDate: new Date(2016, 8-1, 12, 8), endDate: new Date(2016, 8-1, 13, 19) }
 	];
 
 	// Listen to the "daySelect" DOM event, to add a new event
