048bc6fa903df16a0cf5650b0e1b2bf859d764c4
Paul MAILLARDET <polo@etoilenoire>
2016-08-05 00:56:42 +0200

    For DEMO example, events are relative to today's date

--- js/example.js
+++ js/example.js
@@ -15,21 +15,26 @@ planttEx.controller("planttExample", function($scope, $timeout){
 	$scope.autoLock		= true; // To enable the automatic lock of past events
 	$scope.lockMarginDays = 15;	// Number of days before today for the automatic lock to take effect
 
-	// Create the events list
+	// FOR DEMO : using today as a reference to create events, for them to be allways visible
+	var now = new Date();
+
+	// Create the events list (don't use it like this, it's relative for DEMO)
 	$scope.events = [
-		{ id: 1, title: 'Hello World', type: 'normal', startDate: new Date(2016, 6-1, 21), endDate: new Date(2016, 7-1, 4) },
-		{ id: 2, title: 'OK Junior, pent over', type: 'normal', startDate: new Date(2016, 7-1, 11), endDate: new Date(2016, 7-1, 15) },
-		{ id: 3, title: 'Running in the mountain', type: 'urgent', startDate: new Date(2016, 7-1, 18), endDate: new Date(2016, 7-1, 20) },
-		{ id: 4, title: 'July Ruby', type: 'urgent', startDate: new Date(2016, 7-1, 23), endDate: new Date(2016, 7-1, 25) },
-		{ id: 5, title: 'Old one', type: 'urgent', startDate: new Date(2016, 7-1, 17), endDate: new Date(2016, 7-1, 29) },
-		{ id: 6, title: 'Outdated event', type: 'urgent', startDate: new Date(2016, 7-1, 31), endDate: new Date(2016, 8-1, 2) },
-		{ id: 7, title: 'In progress, low priority', type: 'normal', startDate: new Date(2016, 8-1, 2), endDate: new Date(2016, 8-1, 7) },
-		{ id: 8, title: 'Full Week Holidays', type: 'normal', startDate: new Date(2016, 8-1, 8), endDate: new Date(2016, 8-1, 14) },
-		{ id: 9, title: 'Something to do soon', type: 'normal', startDate: new Date(2016, 8-1, 6), endDate: new Date(2016, 8-1, 10) },
-		{ id: 10, title: 'In progress, hi-priority', type: 'urgent', startDate: new Date(2016, 8-1, 4), endDate: new Date(2016, 8-1, 8) },
-		{ id: 11, title: 'Mid-August Fiesta on the beach', type: 'urgent', startDate: new Date(2016, 8-1, 15), endDate: new Date(2016, 8-1, 21) },
-		{ id: 12, title: '1 day', type: 'normal', startDate: new Date(2016, 8-1, 16), endDate: new Date(2016, 8-1, 16) },
-		{ id: 13, title: 'Testing', type: 'urgent', startDate: new Date(2016, 8-1, 12), endDate: new Date(2016, 8-1, 13) }
+		{ id: 1, title: 'Hello World',				type: 'normal', startDate: addDaysToDate(now, -30), endDate: addDaysToDate(now, -22) },
+		{ id: 2, title: 'OK Junior, pent over',		type: 'normal', startDate: addDaysToDate(now, -24), endDate: addDaysToDate(now, -21) },
+		{ id: 3, title: 'Running in the mountain',	type: 'urgent', startDate: addDaysToDate(now, -17), endDate: addDaysToDate(now, -15) },
+		{ id: 4, title: 'July Ruby',				type: 'urgent', startDate: addDaysToDate(now, -12), endDate: addDaysToDate(now, -10) },
+		{ id: 5, title: 'Old one',					type: 'urgent', startDate: addDaysToDate(now, -18), endDate: addDaysToDate(now, -6) },
+		{ id: 6, title: 'Outdated event',			type: 'urgent', startDate: addDaysToDate(now, -4), endDate: addDaysToDate(now, -2) },
+		{ id: 7, title: 'In progress, low priority',type: 'normal', startDate: addDaysToDate(now, -2), endDate: addDaysToDate(now, 3) },
+		{ id: 8, title: 'Full Week Holidays',		type: 'normal', startDate: addDaysToDate(now, 4), endDate: addDaysToDate(now, 10) },
+		{ id: 9, title: 'Something to do soon',		type: 'normal', startDate: addDaysToDate(now, 2), endDate: addDaysToDate(now, 6) },
+		{ id: 10, title: 'In progress, hi-priority',type: 'urgent', startDate: addDaysToDate(now, 0), endDate: addDaysToDate(now, 4) },
+		{ id: 11, title: 'Fiesta on the beach',		type: 'urgent', startDate: addDaysToDate(now, 12), endDate: addDaysToDate(now, 20) },
+		{ id: 12, title: '1 day',					type: 'normal', startDate: addDaysToDate(now, 13), endDate: addDaysToDate(now, 13) },
+		{ id: 13, title: 'Testing',					type: 'urgent', startDate: addDaysToDate(now, 7), endDate: addDaysToDate(now, 8) },
+		{ id: 14, title: 'Near future event',		type: 'normal', startDate: addDaysToDate(now, 30), endDate: addDaysToDate(now, 35) },
+		{ id: 15, title: 'Far future event',		type: 'normal', startDate: addDaysToDate(now, 92), endDate: addDaysToDate(now, 98) }
 	];
 
 	// Listen to the "planttError" DOM event, to do something when an error occurs
